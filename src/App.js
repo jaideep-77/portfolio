b1ca16310e4502b78967383e6647dcf0bf06d248
Jaideep Kular <kularjaideep@gmail.com>
2026-06-02 02:17:34 -0700

    delete skills

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import './App.css';
 import Experience from './components/Experience';
 import Home from './components/Home';
 import Navbar from './components/Navbar';
-import Skills from './components/Skills';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
 
@@ -12,7 +11,6 @@ function App() {
       <Navbar />
       <Home />
       <Experience />
-      <Skills />
       <Projects />
       <Contact />
     </div>
